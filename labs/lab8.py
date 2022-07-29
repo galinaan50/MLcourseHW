fe3736f4b8c2bda98b8e994fd535858e44f454f8
galinaan50 <107210260+galinaan50@users.noreply.github.com>
2022-07-29 12:08:49 +0300

    Add files via upload

---
+++ labs/lab8.py
@@ -0,0 +1,28 @@
+
+
+
+#x=42
+#if x>0 :
+ #   print('"+" * 5')
+  #  print("Positive")
+#else:
+ #  print('"+" * 5')
+ #  print("Negative")
+
+#print('hi')  # This is demo
+# integer number
+#print(42)
+#print(+42)
+#print(-42)
+# Floating points ( реални числа )
+#print(3.14)
+#print(0.1+0.2)
+#print(.236)
+#print(-.236)
+#print(0,1+0,2)
+#print(1_000_000)
+# Division
+print(5/2)
+#print(5//2) #целочислено делене
+#print(5%2)
+
